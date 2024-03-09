da74eda582106f70435cf998eb0b326a957faee3
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2024-03-10 00:55:35 +0400

    salam aleykum millet

---
+++ TestApp/src/main/java/com/testapp/TestApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testapp;
+
+/**
+ *
+ * @author SOFT
+ */
+public class TestApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
